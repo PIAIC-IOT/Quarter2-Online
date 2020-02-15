f6ce497153b3df6f2aaf7025fe0b2e87dc4f49c2
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2020-02-15 12:45:36 +0500

    added code for driving LEDs using apis

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -2,28 +2,26 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux::{entry, iprint, iprintln};
+
+use core::ptr;
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let gpioe = aux::init().1;
 
-    unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+    // Turn on the North LED
+    gpioe.bsrr.write(|w| w.bs9().set_bit());
 
-        // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9;
+    // Turn on the East LED
+    gpioe.bsrr.write(|w| w.bs11().set_bit());
 
-        // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11;
+    // Turn off the North LED
+    gpioe.bsrr.write(|w| w.br9().set_bit());
 
-        // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
-
-        // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
-    }
+    // Turn off the East LED
+    gpioe.bsrr.write(|w| w.br11().set_bit());
 
     loop {}
 }
+
