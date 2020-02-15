2ca1ce7cc17979e9bd25917d4f38931bcfc9a6ae
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2020-02-15 13:04:09 +0500

    reading a single byte from LSM303 using I2C

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -19,23 +19,43 @@ fn main() -> ! {
     // Stage 1: Send the address of the register we want to read to the
     // magnetometer
     {
-        // TODO Broadcast START
+        // Broadcast START
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Write
+        i2c1.cr2.write(|w| {
+            w.start().set_bit();
+            w.sadd1().bits(MAGNETOMETER);
+            w.rd_wrn().clear_bit();
+            w.nbytes().bits(1);
+            w.autoend().clear_bit()
+        });
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Write
+        // Wait until we can send more data
+        while i2c1.isr.read().txis().bit_is_clear() {}
 
-        // TODO Send the address of the register that we want to read: IRA_REG_M
+        // Send the address of the register that we want to read: IRA_REG_M
+        i2c1.txdr.write(|w| w.txdata().bits(IRA_REG_M));
+
+        // Wait until the previous byte has been transmitted
+        while i2c1.isr.read().tc().bit_is_clear() {}
     }
 
     // Stage 2: Receive the contents of the register we asked for
     let byte = {
-        // TODO Broadcast RESTART
+        // Broadcast RESTART
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Read
+        i2c1.cr2.modify(|_, w| {
+            w.start().set_bit();
+            w.nbytes().bits(1);
+            w.rd_wrn().set_bit();
+            w.autoend().set_bit()
+        });
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Read
+        // Wait until we have received the contents of the register
+        while i2c1.isr.read().rxne().bit_is_clear() {}
 
-        // TODO Receive the contents of the register
+        // Broadcast STOP (automatic because of `AUTOEND = 1`)
 
-        // TODO Broadcast STOP
-        0
+        i2c1.rxdr.read().rxdata().bits()
     };
 
     // Expected output: 0x0A - 0b01001000
